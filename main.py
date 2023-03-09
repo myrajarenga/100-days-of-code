24393e08faf00d5bd33f106fa92c7c1dec00169f
Myra Jarenga <105559715+myrajarenga@users.noreply.github.com>
2023-03-09 14:30:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello Replit")
+print("and how are you")
+print("Well we")
+print("just use more lines")
+print("of code")
+print("""Anything that starts
+with three quotes, and ends
+in three quotes can span
+many lines and even contain " symbols
+within it without freaking anything out!""")
