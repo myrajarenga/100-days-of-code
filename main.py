bb9c2199f2d706e4eceaf8427bd92642bc12ee62
Myra Jarenga <105559715+myrajarenga@users.noreply.github.com>
2023-03-09 15:11:45 +0000

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-print("Hello Replit")
-print("and how are you")
-print("Well we")
-print("just use more lines")
-print("of code")
-print("""Anything that starts
-with three quotes, and ends
-in three quotes can span
-many lines and even contain " symbols
-within it without freaking anything out!""")
+print("Myra jarenga.")
+print("Today the 9th of March 2023.")
+print("I am signing up for Replit's 100 days of Python challenge!")
+print("I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.")
+print("I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!")
+print("I am feeling excited.")
+print("You can follow my progress at replit.com/@MyraJarenga")
