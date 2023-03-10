9122e5529855d19a038f4544c39b0e568f2051da
Myra Jarenga <105559715+myrajarenga@users.noreply.github.com>
2023-03-10 08:12:41 +0000

    taking user input

---
+++ print.py
@@ -0,0 +1,11 @@
+print("Myra jarenga.")
+print("Today the 9th of March 2023.")
+print("I am signing up for Replit's 100 days of Python challenge!")
+print(
+  "I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day."
+)
+print(
+  "I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!"
+)
+print("I am feeling excited.")
+print("You can follow my progress at replit.com/@MyraJarenga")
