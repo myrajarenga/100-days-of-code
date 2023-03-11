0de49fa3f5c563bf650d22833d732269538d1b7c
Myra Jarenga <105559715+myrajarenga@users.noreply.github.com>
2023-03-11 20:02:59 +0000

    How to use if  and esle

---
+++ if_else.py
@@ -0,0 +1,15 @@
+
+print("Marvel movie character creator ---")
+
+question = input("Do you like 'hanging around'?: ")
+
+if question == "No":
+  print("Then you are not Spider-man")
+question2 = input("Do you have a 'gravaliy' voice?: ")
+if question2 == "No":
+  print("Aww, then you're not Korg")
+question3 = input("Do you often feel 'Marvelous': ")
+if question3 == "Yes":
+  print("Aha! you're captain Marvel! Hi!")
+else:
+  print()
