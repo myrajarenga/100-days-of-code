f9572bc70c6df3bc9202ab481f816afb470a97b7
Myra Jarenga <105559715+myrajarenga@users.noreply.github.com>
2023-03-14 07:13:27 +0000

    nestefd if stetements

---
+++ nested_if.py
@@ -0,0 +1,13 @@
+best_tvshow = input("Whats you best tv_show?" )
+if best_tvshow == "Blacklist":
+  print("Ugh!, Why?")
+  favourite_character = input("Whats you favourite character?" )
+  if favourite_character == "Raymond Reddington":
+    print("Right answere")
+  else:
+    print("Nah, wrong answere favourite character is Raymond Reddington")
+
+elif best_tvshow == "If loving you is wrong":
+  print("Aww, sad times")
+else:
+  print("You need to find better movies to watch")
---
+++ nested_if2.py
@@ -0,0 +1,14 @@
+favuorite_anime = input("Whats your favourite Anime? ")
+if favuorite_anime == "One piece":
+  print("Oh really!. Name me any of the characters? ")
+  name_of_characters = "Nami"
+  if name_of_characters == "Nami":
+    print("You got that by pure chance, ok then whats her job on the ship?")
+  job = input("job on the ship ")
+  if job == "Navigotor":
+    print("Whats her first bounty ")
+  bounty = input("first bouty ")
+  if bounty == "Ummm...":
+    print("See! FAKE One piece fan ")
+else:
+  print("See! FAKE One piece fan ")
